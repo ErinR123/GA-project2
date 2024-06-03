a5166dbaa6ed8895c38a5441f0f1b3a50ebd5ef4
Erin Robinson <r.erin2546@gmail.com>
2024-06-03 14:58:20 +0930

    removing day of week options part 2

--- routes/mindfulness.js
+++ routes/mindfulness.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const mindfulness = require("../models/mindfulness");
 const mindfulnessCtrl = require("../controllers/mindfulness");
 
-router.get("/", mindfulnessCtrl.rendermindfulness);
+// router.get("/", mindfulnessCtrl.rendermindfulness);
 
 router.post("/", mindfulnessCtrl.add);
 router.delete("/:mindfulnessId", mindfulnessCtrl.deletemindfulness);
